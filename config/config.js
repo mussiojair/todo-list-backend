76fc6712c93a75af34ff49a57cf7aac69a400fb0
mussiojair <mussiojair@gmail.com>
2024-09-30 00:49:43 -0600

    Support: validates apikey

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const config = {
     serverPort: process.env.PORT ?? 3000, // 3000 by default
+    apiKey: process.env.API_KEY,
 };
 
 module.exports = { config };
--- middlewares/api.handler.js
+++ middlewares/api.handler.js
@@ -2,6 +2,7 @@ const boom = require("@hapi/boom");
 const { config } = require("../config/config");
 
 function checkApi(req, res, next) {
+    console.log(config.apiKey, req.headers['api']);
     if (config.apiKey == req.headers['api']) {
         next();
     } else {
