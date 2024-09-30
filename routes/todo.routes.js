5cb668a99bf7c27db0f9779ec06a53e04fbdc9c6
mussiojair <mussiojair@gmail.com>
2024-09-29 23:51:26 -0600

    Support: handle error responses through Boom

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -9,27 +9,30 @@ const service = new TodoService();
 /**
  * Retrieve all TODO existing in database
  */
-router.get('/', async (req, res) => {
-    try {
-        const items = await service.find();
-        res.json(items);
-    } catch(error) {
-        res.status(500).json({message: "Error retrieving all items"});
+router.get('/', 
+    async (req, res, next) => {
+        try {
+            const items = await service.find();
+            res.json(items);
+        } catch(error) {
+            // res.status(500).json({message: "Error retrieving all items"});
+            next(error);
+        }
     }
-});
+);
 
 /**
  * Retrieve details for a specific TODO existing in database.
  */
 router.get('/:id', 
     validatorHandler(getTodoSchema, 'params'),
-    async (req, res) => {
+    async (req, res, next) => {
         try {
             const { id } = req.params;
             const items = await service.findOne(id);
             res.json(items);
         } catch(error) {
-            res.status(404).json({message: error.message});
+            next(error);
         }
     }
 );
@@ -39,7 +42,7 @@ router.get('/:id',
  */
 router.post('/', 
     validatorHandler(createTodoSchema, 'body'),
-    async (req, res) => {
+    async (req, res, next) => {
         try {
             const data = req.body;
             const items = await service.create(data);
@@ -56,14 +59,15 @@ router.post('/',
 router.patch('/:id',
     validatorHandler(getTodoSchema, 'params'),
     validatorHandler(updateTodoSchema, 'body'),
-    async (req, res) => {
+    async (req, res, next) => {
         try {
             const { id } = req.params;
             const data = req.body;
             const updatedData = await service.update(id, data);
             res.status(201).json(updatedData);
         } catch(error) {
-            res.status(500).json({message: "PATCH operation failed. Update couldn't be done."});
+            // res.status(500).json({message: "PATCH operation failed. Update couldn't be done."});
+            next(error);
         }
     }
 );
@@ -73,13 +77,14 @@ router.patch('/:id',
  */
 router.delete('/:id', 
     validatorHandler(deleteTodoSchema, 'params'),
-    async (req, res) => {
+    async (req, res, next) => {
         try {
             const { id } = req.params;
             const result = await service.delete(id);
             res.json(result);
         } catch(error) {
-            res.status(500).json({ message: "DELETE operation failed. Delete on user {id} couldn't be done."});
+            // res.status(500).json({ message: "DELETE operation failed. Delete on user {id} couldn't be done."});
+            next(error);
         }
     }
 );
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routerApi = require('./routes');
+const { boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 
@@ -8,6 +9,9 @@ app.use(express.json());
 
 // routes
 routerApi(app);
+
+// support boom error handler
+app.use(boomErrorHandler);
 
 app.listen(3000, () => {
     console.log('Node app is running in port 3000')
